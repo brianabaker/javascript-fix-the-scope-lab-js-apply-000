85c45f5ba90b5db07760d87d8d5dd1b3cf7b34c1
Briana Baker <brianaclairbaker@gmail.com>
2017-11-14 12:27:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,10 +5,7 @@ function myAnimal() {
 }
 
 function yourAnimal() {
-  // How can we make sure that this function
-  // and the above function both pass?
-  // P.S.: You can't just hard-code 'cat' below
-
+  myAnimal()l
   return animal
 }
 
